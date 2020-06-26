b9d460cc66c826cb749e34363f184e80d625a04b
Patrick Assaf <patrickassaf96@gmail.com>
2020-06-26 21:12:07 +0300

    Check if email already exists in the registration resolver

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -62,6 +62,12 @@ export class UserResolver {
         @Arg('email') email: string,
         @Arg('password') password: string
     ) {
+        const user = await User.findOne({ where: { email } });
+
+        if(user) {
+            return false;
+        }
+
         const hashedPassword = await hash(password, 12);
 
         try {
