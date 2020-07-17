2f8fc65991f60b8202efb433ece1dea6e9b36926
Patrick Assaf <patrickassaf96@gmail.com>
2020-07-17 12:52:13 +0300

    Create route components for registration, login and index pages

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 
 export const Routes: React.FC = () => {
   return <BrowserRouter>
     <Switch>
-      <Route exact path="/" render={() => <div>Hello</div>} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
-}
+};
