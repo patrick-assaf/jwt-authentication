27c882984dd69a4684cf36612a07e90892b4aaa5
Patrick Assaf <patrickassaf96@gmail.com>
2020-07-18 14:35:30 +0300

    Build react form for registration page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
 
 }
 
 export const Register: React.FC<Props> = ({}) => {
-    return <div>Register Page</div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <form onSubmit={e => {
+            e.preventDefault();
+            console.log('Form Submitted!');
+            console.log(email, password);
+        }}>
+            <div>
+                <input 
+                    type="email" 
+                    value={email} 
+                    placeholder="email" 
+                    onChange={e => {
+                        setEmail(e.target.value)
+                    }} 
+                />
+            </div>
+            <div>
+                <input 
+                    type="password" 
+                    value={password} 
+                    placeholder="password" 
+                    onChange={e => {
+                        setPassword(e.target.value)
+                    }} 
+                />
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    );
 };
