5fc556f4edeacdfb67040a886304f6a8c47bc7db
Patrick Assaf <patrickassaf96@gmail.com>
2020-07-23 12:13:05 +0300

    Display all registered users on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useUsersQuery } from '../generated/graphql';
 
 interface Props {
 
 }
 
 export const Home: React.FC<Props> = ({}) => {
-    return <div>Home Page</div>;
+    const {data} = useUsersQuery({fetchPolicy: 'network-only'});
+
+    if(!data) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <div>Users:</div>
+            <ul>
+                {data.users.map(x => {
+                    return (
+                        <li key={x.id}>
+                            {x.email}, {x.id}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
